e033310ef4dd044a788a3a29476a255cfbab0da3
Victor Acuña Uribe <vc.acunauribe@gmail.com>
2021-10-20 21:57:00 -0300

    End of section 4

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,8 +1,13 @@
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { useState } from "react";
+import { Route } from "react-router";
 
-import Catalog from '../../features/catalog/Catalog';
 import Header from './Header';
+import Catalog from '../../features/catalog/Catalog';
+import HomePage from '../../features/home/HomePage';
+import AboutPage from '../../features/about/AboutPage';
+import ContactPage from '../../features/contact/ContactPage';
+import ProductDetails from '../../features/catalog/ProductDetails';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -25,7 +30,11 @@ function App() {
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Catalog />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/catalog" component={Catalog} />
+        <Route path="/catalog/:id" component={ProductDetails} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/contact" component={ContactPage} />
       </Container>
     </ThemeProvider>
   );
